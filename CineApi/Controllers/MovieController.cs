c570feb844a7780ec2534795c4c0975b908a0fcc
raloliver <raloliver.ws@gmail.com>
2023-09-01 17:29:37 +0100

    Add endpoint to register a new movie

---
+++ CineApi/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CineApi.Models;
+
+namespace CineApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController: ControllerBase
+{
+    // static to remain the class to the object
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+    public void CreateMovie([FromBody] Movie movie)
+    {
+        movies.Add(movie);
+        Console.WriteLine(movie.Title);
+        Console.WriteLine(movie.Genre);
+        Console.WriteLine(movie.Duration);
+    }
+}
---
+++ CineApi/Models/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace CineApi.Models;
+
+public class Movie
+{
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public int Duration { get; set; }
+}
