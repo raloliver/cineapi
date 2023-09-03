c64fd1e170a4565a03c2e9820389d5768821e9d1
raloliver <raloliver.ws@gmail.com>
2023-09-03 07:12:27 +0100

    Add validations and the messages for each one

--- CineApi/Models/Movie.cs
+++ CineApi/Models/Movie.cs
@@ -1,8 +1,15 @@
-﻿namespace CineApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CineApi.Models;
 
 public class Movie
 {
+    [Required(ErrorMessage = "A movie must have a title!")]
     public string Title { get; set; }
+    [Required(ErrorMessage = "A movie must have a genre!")]
+    [MaxLength(50, ErrorMessage = "The genre max length is 50 chars!")]
     public string Genre { get; set; }
+    [Required(ErrorMessage = "A movie must have some time in minute!")]
+    [Range(70, 600, ErrorMessage ="The duration of the movie must be between 70 and 600 minutes")]
     public int Duration { get; set; }
 }
